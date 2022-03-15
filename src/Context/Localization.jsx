27dc7cf0520289dd450982e70d0e36e8977d7e97
Boburjon Urazbayev <bobur7850@gmail.com>
2022-03-15 17:46:22 +0500

    Add localStorage

--- src/Context/Localization.jsx
+++ src/Context/Localization.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 const Context = React.createContext(null);
 
-function Provider({children}) {
-    const [lang, setLang] = React.useState("uz")
-    return <Context.Provider value={{lang, setLang}}>{children}</Context.Provider>
+function Provider({ children }) {
+  const [lang, setLang] = React.useState(
+    window.localStorage.getItem("lang") || "en"
+  );
+
+  React.useEffect(() => {
+    if (lang) {
+      window.localStorage.setItem("lang", lang);
+    } else {
+      window.localStorage.removeItem("lang", lang);
+    }
+  }, [lang]);
+
+  return (
+    <Context.Provider value={{ lang, setLang }}>{children}</Context.Provider>
+  );
 }
 
 export { Context, Provider };
